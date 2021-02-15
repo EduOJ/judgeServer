7c56a4407f13d3d1c64a2ca30790f0b861dabdb6
Leo Lu <luyuxuanleo@gmail.com>
2021-02-16 03:10:58 +0800

    Fix CI

--- base/file_test.go
+++ base/file_test.go
@@ -9,15 +9,15 @@ import (
 
 func TestRemoveCache(t *testing.T) {
 	// Not parallel
-	assert.Nil(t, os.RemoveAll("../test_file/test_remove_buffer"))
+	assert.NoError(t, os.RemoveAll("../test_file/test_remove_buffer"))
 	t.Cleanup(func() {
 		_ = os.RemoveAll("../test_file/test_remove_buffer")
 	})
 	viper.Set("path.scripts", "../test_file/test_remove_buffer/scripts")
 	viper.Set("path.runs", "../test_file/test_remove_buffer/runs")
-	assert.Nil(t, os.MkdirAll(viper.GetString("path.scripts"), 0777))
-	assert.Nil(t, os.MkdirAll(viper.GetString("path.runs"), 0777))
-	assert.Nil(t, RemoveCache())
+	assert.NoError(t, os.MkdirAll(viper.GetString("path.scripts"), 0777))
+	assert.NoError(t, os.MkdirAll(viper.GetString("path.runs"), 0777))
+	assert.NoError(t, RemoveCache())
 	_, err := os.Stat(viper.GetString("path.scripts"))
 	assert.True(t, os.IsNotExist(err))
 	_, err = os.Stat(viper.GetString("path.runs"))
