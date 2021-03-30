1f3e0c158dd3719e6d92025cbc2b9e4e6cc190e0
Leo Lu <luyuxuanleo@gmail.com>
2021-03-30 23:02:16 +0800

    Dev sysroot

--- build/sysroot.sh
+++ build/sysroot.sh
@@ -4,7 +4,7 @@ mkdir sysroot-no-stl
 sudo debootstrap bionic ./sysroot-no-stl
 cp /etc/apt/sources.list sysroot-no-stl/etc/apt/sources.list
 sudo chroot sysroot-no-stl apt-get update
-sudo chroot sysroot-no-stl apt-get install -y libstdc++-9-dev
+sudo chroot sysroot-no-stl apt-get install -y clang-10
 sudo mv ./sysroot-no-stl/usr/include/c++/9/vector ./sysroot-no-stl/usr/include/c++/9/vector_forbidden
 sudo mv ./sysroot-no-stl/usr/include/c++/9/deque ./sysroot-no-stl/usr/include/c++/9/deque_forbidden
 sudo mv ./sysroot-no-stl/usr/include/c++/9/list ./sysroot-no-stl/usr/include/c++/9/list_forbidden
