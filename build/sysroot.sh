75f99c6b0113f3391c4c1c522e5cc8a971086437
Leo Lu <luyuxuanleo@gmail.com>
2021-03-31 16:24:17 +0800

    Ban algorithm in no-stl mode.

--- build/sysroot.sh
+++ build/sysroot.sh
@@ -5,8 +5,8 @@ sudo debootstrap bionic ./sysroot-no-stl
 sudo cp /etc/apt/sources.list sysroot-no-stl/etc/apt/sources.list
 sudo chroot sysroot-no-stl apt-get update
 sudo chroot sysroot-no-stl apt-get install -y libstdc++-9-dev
-sudo mv ./sysroot-no-stl/usr/include/c++/9/vector ./sysroot-no-stl/usr/include/c++/9/vector_forbidden
 sudo mv ./sysroot-no-stl/usr/include/c++/9/deque ./sysroot-no-stl/usr/include/c++/9/deque_forbidden
 sudo mv ./sysroot-no-stl/usr/include/c++/9/list ./sysroot-no-stl/usr/include/c++/9/list_forbidden
+sudo mv ./sysroot-no-stl/usr/include/c++/9/algorithm ./sysroot-no-stl/usr/include/c++/9/algorithm_forbidden
 sudo tar -czvf sysroot.tgz ./sysroot-no-stl
 sudo rm -r sysroot-no-stl
