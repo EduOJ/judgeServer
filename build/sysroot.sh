ef11e116c2ce4fef8731ad3cfe2e7c37fd9fa012
Leo Lu <luyuxuanleo@gmail.com>
2021-04-11 19:11:41 +0800

    Fix sysroot version

--- build/sysroot.sh
+++ build/sysroot.sh
@@ -4,9 +4,9 @@ mkdir sysroot-no-stl
 sudo debootstrap bionic ./sysroot-no-stl
 sudo cp /etc/apt/sources.list sysroot-no-stl/etc/apt/sources.list
 sudo chroot sysroot-no-stl apt-get update
-sudo chroot sysroot-no-stl apt-get install -y libstdc++-9-dev
-sudo mv ./sysroot-no-stl/usr/include/c++/9/deque ./sysroot-no-stl/usr/include/c++/9/deque_forbidden
-sudo mv ./sysroot-no-stl/usr/include/c++/9/list ./sysroot-no-stl/usr/include/c++/9/list_forbidden
-sudo mv ./sysroot-no-stl/usr/include/c++/9/algorithm ./sysroot-no-stl/usr/include/c++/9/algorithm_forbidden
+sudo chroot sysroot-no-stl apt-get install -y libstdc++-7-dev
+sudo mv ./sysroot-no-stl/usr/include/c++/7/deque ./sysroot-no-stl/usr/include/c++/7/deque_forbidden
+sudo mv ./sysroot-no-stl/usr/include/c++/7/list ./sysroot-no-stl/usr/include/c++/7/list_forbidden
+sudo mv ./sysroot-no-stl/usr/include/c++/7/algorithm ./sysroot-no-stl/usr/include/c++/7/algorithm_forbidden
 sudo tar -czvf sysroot.tgz ./sysroot-no-stl
 sudo rm -r sysroot-no-stl
